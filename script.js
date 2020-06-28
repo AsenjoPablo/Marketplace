f995ac16c588aa0591396fa282af7968ea2b6476
Pablo Asenjo <asenjo.serrano@gmail.com>
2020-06-28 13:52:02 +0200

    Updated code pre-testing

--- script.js
+++ script.js
@@ -49,15 +49,6 @@ $(document).ready(function() {
     var plantQty = 0;
     var tvbenchQty = 0;
 
-    // REMOVE BUTTON DELETES ACTUAL TR
-    var removeButton = '<button class="removeBtn btn btn-danger">REMOVE</button>';
-
-    $(document).on('click', '.removeBtn', function(){ 
-        $(this).parent().parent().remove();
-   })
-
-   // TODO REMOVE TOTALPRICE - PRODUCT
-
     // BUY ITEMS LIST
     buyChairBtn.click (function () {
         totalCost += chairPrice;
@@ -67,7 +58,7 @@ $(document).ready(function() {
         //table add functions
         counterTable++;
         chairQty++;
-        $("tbody").append('<tr><th class="counterProd" scope="row">' + counterTable + '</th> <td>Chair</td><td>'+chairPrice+'</td><td>' + chairQty + '</td><td>' + removeButton + '</td></tr>');
+        $("tbody").append('<tr><th scope="row">' + counterTable + '</th> <td>Chair</td><td class="price">'+chairPrice+'</td><td class="qty">' + chairQty + '</td><td>' + removeButton + '</td></tr>');
     })
 
     buyTableBtn.click (function () {
@@ -78,7 +69,7 @@ $(document).ready(function() {
         //table add functions
         counterTable++;
         tableQty++;
-        $("tbody").append('<tr><th scope="row">' + counterTable + '</th> <td>Table</td><td>'+tablePrice+'</td><td>' + tableQty + '</td><td>' + removeButton + '</td></tr>');
+        $("tbody").append('<tr><th scope="row">' + counterTable + '</th> <td>Table</td><td class="price">'+tablePrice+'</td><td>' + tableQty + '</td><td>' + removeButton + '</td></tr>');
     })
 
     buyBedBtn.click (function () {
@@ -89,7 +80,7 @@ $(document).ready(function() {
         //table add functions
         counterTable++;
         bedQty++;
-        $("tbody").append('<tr><th scope="row">' + counterTable + '</th> <td>Bed</td><td>'+bedPrice+'</td><td>' + bedQty + '</td><td>' + removeButton + '</td></tr>');   
+        $("tbody").append('<tr><th scope="row">' + counterTable + '</th> <td>Bed</td><td class="price">'+bedPrice+'</td><td>' + bedQty + '</td><td>' + removeButton + '</td></tr>');   
     })
 
     buyBookcaseBtn.click (function () {
@@ -100,7 +91,7 @@ $(document).ready(function() {
         //table add functions
         counterTable++;
         bookcaseQty++;
-        $("tbody").append('<tr><th scope="row">' + counterTable + '</th> <td>Bookcase</td><td>'+bookcasePrice+'</td><td>' + bookcaseQty + '</td><td>' + removeButton + '</td></tr>');   
+        $("tbody").append('<tr><th scope="row">' + counterTable + '</th> <td>Bookcase</td><td class="price">'+bookcasePrice+'</td><td>' + bookcaseQty + '</td><td>' + removeButton + '</td></tr>');   
     })
 
     buyPlantBtn.click (function () {
@@ -111,7 +102,7 @@ $(document).ready(function() {
         //table add functions
         counterTable++;
         plantQty++;
-        $("tbody").append('<tr><th scope="row">' + counterTable + '</th> <td>Plant</td><td>'+plantPrice+'</td><td>' + plantQty + '</td><td>' + removeButton + '</td></tr>');  
+        $("tbody").append('<tr><th scope="row">' + counterTable + '</th> <td>Plant</td><td class="price">'+plantPrice+'</td><td>' + plantQty + '</td><td>' + removeButton + '</td></tr>');  
     })
 
     buyTVBenchBtn.click (function () {
@@ -122,7 +113,7 @@ $(document).ready(function() {
         //table add functions
         counterTable++;
         tvbenchQty++;
-        $("tbody").append('<tr><th scope="row">' + counterTable + '</th> <td>TV bench</td><td>'+tvbenchPrice+'</td><td>' + tvbenchQty + '</td><td>' + removeButton + '</td></tr>');  
+        $("tbody").append('<tr><th scope="row">' + counterTable + '</th> <td>TV bench</td class="price"><td>'+tvbenchPrice+'</td><td>' + tvbenchQty + '</td><td>' + removeButton + '</td></tr>');  
     })
 
     // total cost of products
@@ -132,8 +123,41 @@ $(document).ready(function() {
     function updateTotalCost () {
         $("#total-cost").html("Total cost: " + totalCost + "€");
     }
-
     
+    // REMOVE BUTTON DELETES ACTUAL TR
+    var removeButton = '<button class="removeBtn btn btn-danger">REMOVE</button>';
+    $(document).on('click', '.removeBtn', function(){ 
+
+        //remove price from total count
+        var removePrice = $(this).parent().siblings(".price").html();
+        totalCost -= removePrice;
+        updateTotalCost();
+
+        //remove element row
+        $(this).parent().parent().remove();
+    })
+
+
+    //**********************************EXPERIMENTATION PLAYGROUND**************************************
+    // global builder
+    function Item (name, price) {
+        this.name = name;
+        this.price = price;
+        this.qty = 0;
+    }
+
+    //test builder
+    var test = new Item ("test", 10);
+
+    $("#test-add").click(function () {
+        addTest(test);  
+    })
+
+    function addTest (testObj) {
+        $("tbody").append('<tr><th scope="row">' + testObj.id + '</th> <td>' + testObj.name +'</td class="price"><td>'+testObj.price+'</td><td>' + testObj.qty + '</td><td>' + removeButton + '</td></tr>');  
+    }
+    addTest(test)
+
 
 
 });
